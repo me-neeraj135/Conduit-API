cf7c5004a6f078713ee3ff0498a025c0db4ca75a
neeraj135 <me.neeraj1455.com>
2024-04-02 02:13:23 +0530

    add database

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,10 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.status(200).json({ message: "Welcome to Blogging  API...!" });
+  res.status(200).json({
+    message:
+      "Welcome to Blogging  API...! Here you can post your Blog and perform all CRUD operation",
+  });
 });
 
 module.exports = router;
